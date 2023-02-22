4ec35d559840ea10663ed9ad4efef0d0a3635234
Repaldi <repaldi25@gmail.com>
2023-02-22 09:18:31 +0700

    Ok

--- resources/views/frontend/explorer.blade.php
+++ resources/views/frontend/explorer.blade.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-lg-3">
-                <span class="mt-2" style=" font-size: 18px; font-weight: bold; color: #37517e;">Cari Data Ikan :</span>  
+                <span class="mt-2" style=" font-size: 18px; font-weight: bold; color: #37517e;">Cari Data Ikan :</span>
                 <form>
                     <div class="input-group ">
                         <input type="text" class="form-control" placeholder="Kata Kunci ..." name="search">
@@ -25,7 +25,7 @@
                 </form>
             </div>
             <div class="col-md-9 col-lg-9" id="nav-abjad">
-            <span class="mt-2" style=" font-size: 18px; font-weight: bold; color: #37517e;">Berdasarkan Abjad: </span> 
+            <span class="mt-2" style=" font-size: 18px; font-weight: bold; color: #37517e;">Berdasarkan Abjad: </span>
                 <ol class="mb-2">
                     <li><a href="#" data-abjad="all" class="abjad"> All </a></li>
                     @foreach(range('A','Z') as $abjad)
@@ -45,16 +45,16 @@
         </div>
         <div class="row" id="list-ikan-holder">
             @forelse($data_spesies as $item)
-          
+
             <div class="col-xl-4">
-            
+
                 <div class="blog-item">
-               
+
                     <div class="card" style="width: 18rem;" >
                     <img src="{{$item->getImage()}}" class="card-img-top" alt="{{$item->getImage()}}">
                     <div class="card-body">
                         <h5 class="card-title">{!! $item->nama_umum !!}</h5>
-                        <p class="card-text">( <em> {!! $item->nama_latin !!} </em> )</p>
+                        <p class="card-text">( {!! $item->nama_latin !!}  )</p>
                         @php
                             $trancated = Str::of($item->deskripsi)->limit(200);
                         @endphp
@@ -65,10 +65,10 @@
                     </div>
                     </div>
                 </div>
-         
+
             </div>
             @empty
-            
+
             <p><center> Data Tidak Ditemukan </center> </p>
             @endforelse
             <div>
@@ -81,28 +81,28 @@
 @section("linkfooter")
 <script>
     $(document).ready(function () {
-        $(".abjad").click(function (e) { 
+        $(".abjad").click(function (e) {
             e.preventDefault();
-            let abjad = $(this).data("abjad"); 
+            let abjad = $(this).data("abjad");
             url = "/explorer?abjad="+abjad
             // let param = {
             //     abjad: abjad
             // }
             // doAjax(param)
             window.location.href = url
-            
+
         });
-        // $(".abjad").click(function (e) { 
+        // $(".abjad").click(function (e) {
         //     e.preventDefault();
-        //     let abjad = $(this).data("abjad"); 
+        //     let abjad = $(this).data("abjad");
         //     let param = {
         //         abjad: abjad
         //     }
         //     doAjax(param)
-            
+
         // });
 
-        $(".btn-search").click(function (e) { 
+        $(".btn-search").click(function (e) {
             e.preventDefault();
             let keyword = $("input[name='search']").val();
             url = "/explorer?search="+keyword
@@ -112,7 +112,7 @@
             // doAjax(param)
             window.location.href = url
         });
-        // $(".btn-search").click(function (e) { 
+        // $(".btn-search").click(function (e) {
         //     e.preventDefault();
         //     let keyword = $("input[name='search']").val();
         //     let param = {
