471b3cf82b2da46ad88ca134f3a7bb2bd127e2bc
Repaldi <repaldi25@gmail.com>
2023-02-22 09:20:13 +0700

    Ok

--- resources/views/frontend/explorer-detail.blade.php
+++ resources/views/frontend/explorer-detail.blade.php
@@ -17,7 +17,7 @@
                 <table class="table table-striped">
                 <thead>
                     <h3>Data Informasi {!! $data->nama_umum ?? '' !!} </h3>
-                    <h5><em> ( {!! $data->nama_latin ?? '' !!} )</em></h5>
+                    <h5> {!! $data->nama_latin ?? '' !!} </h5>
                     <tr>
                         <th colspan="3"><center><img src="{{$data->getImage()}}"></center></th>
                     </tr>
