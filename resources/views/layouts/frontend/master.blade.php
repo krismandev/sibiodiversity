f1f01b7e9a569688e3b04a99ca969be5504b1162
krismandev <krismanpratama@gmail.com>
2023-03-23 23:56:08 +0700

    tampilan berita di index

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -20,9 +20,9 @@
 	<link rel="stylesheet" href="{{asset('assets_frontend/css/animate.css')}}"/>
 	<link rel="stylesheet" href="{{asset('assets_frontend/css/owl.carousel.css')}}"/>
 	<link rel="stylesheet" href="{{asset('assets_frontend/css/style.css')}}"/>
-    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 	{{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.css"/> --}}
